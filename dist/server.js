70d689087fba2573dd4b6f139f9277a4bbb3ff23
Francisco <frandelosantoss12@gmail.com>
2021-09-09 15:32:36 -0300

    make it better

--- dist/server.js
+++ dist/server.js
@@ -12,12 +12,11 @@ var routes_1 = __importDefault(require("./routes"));
 var app = (0, express_1.default)();
 // @ts-ignore
 var port = process.env.PORT || 3000;
-var host = process.env.HOST;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
-app.listen(port, host, function () {
-    logger_1.default.info("Server listening at http://" + host + ":" + port);
+app.listen(port, function () {
+    logger_1.default.info("Server listening at " + port + " port.");
     (0, connect_1.default)();
     (0, routes_1.default)(app);
 });
--- src/server.ts
+++ src/server.ts
@@ -9,14 +9,13 @@ const app = express()
 
 // @ts-ignore
 const port = process.env.PORT as number || 3000
-const host = process.env.HOST as string
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-app.listen(port, host, () => {
-    log.info(`Server listening at http://${host}:${port}`)
+app.listen(port, () => {
+    log.info(`Server listening at ${port} port.`)
 
     connect()
 
