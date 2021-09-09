99148cba666e3387ceb4956ffe0b5eec28ffb4df
Francisco <frandelosantoss12@gmail.com>
2021-09-09 02:20:49 -0300

    Fix

--- dist/config/default.js
+++ dist/config/default.js
@@ -1,6 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
+module.exports = {
     host: 'localhost',
     port: 4000,
     dbUri: "mongodb+srv://Frandols:mariapaz@frandolscluster.va9cg.mongodb.net/FrandolsMedia?retryWrites=true&w=majority",
