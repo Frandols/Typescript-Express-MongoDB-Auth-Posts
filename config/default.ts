a2d989ce4d138a458b9032bf08b623c9386b3c6d
Francisco <frandelosantoss12@gmail.com>
2021-09-09 02:02:30 -0300

    Fix

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default {
     host: 'localhost',
-    port: process.env.PORT,
+    port: 4000,
     dbUri: "mongodb+srv://Frandols:mariapaz@frandolscluster.va9cg.mongodb.net/FrandolsMedia?retryWrites=true&w=majority",
     saltWorkFactor: 10,
     privateKey: '673e2092e54847829d5fff1204827f7ddd72d09ccd317a3475457e9cbed32953654644ae9561d6fb39c34846e5a7fe158c7a97bd5549fe68c8ef1ef1de51c16a'
